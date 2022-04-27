df2436b5b59a4567b99569e2a3eb8cbb5ad30503
nap-owen <nap.owenmontemor@gmail.com>
2022-04-27 20:01:04 +0800

    http updated

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://my-json-server.typicode.com/nap-owen/vue-mastery',
+  baseURL: 'https://my-json-server.typicode.com/nap-owen/vue-mastery',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
